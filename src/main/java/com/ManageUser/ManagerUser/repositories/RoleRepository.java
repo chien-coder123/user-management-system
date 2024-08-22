a5c1e6ffc302cf5421d57f52ff4fae0ccc121ffa
chien-coder123 <hoangminhchien655@gmail.com>
2024-08-22 15:47:33 +0700

    Them 2 ham trong RoleRepository.java

--- src/main/java/com/ManageUser/ManagerUser/repositories/RoleRepository.java
+++ src/main/java/com/ManageUser/ManagerUser/repositories/RoleRepository.java
@@ -4,8 +4,12 @@ import com.ManageUser.ManagerUser.models.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface RoleRepository extends JpaRepository<Role,Long> {
   boolean existsByName(String name);
-
+  List<Role> findAllByNameIn(List<String> names);
+  Optional<Role> findByName(String name);
 }
